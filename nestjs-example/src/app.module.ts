74c2c177f33eadc4830707c879b24226bdf62bc7
Dang Giang <gianggiang1512@gmail.com>
2023-07-03 22:32:06 +0700

    add typeorm

--- nestjs-example/src/app.module.ts
+++ nestjs-example/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/User';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [CustomersModule, UsersModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'tutorial_db',
+    entities: [User],
+    synchronize: true,
+  })],
   controllers: [],
   providers: [],
 })
---
+++ nestjs-example/src/typeorm/User.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn({
+        type: 'bigint',
+        name: 'user_id',
+    })
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column({
+        name: 'email_address',
+        nullable: false,
+    })
+    emailAddress: string;
+
+    @Column()
+    password: string;
+}
